5df2dc8190c886375b1ee890632ceba9cc5b2d33
aminmb37 <amin.mb37@gmail.com>
2020-11-11 00:12:34 +0100

    Implementing Beer Order Validation Request.

--- src/main/java/com/amin/beer/order/service/services/BeerOrderManagerImpl.java
+++ src/main/java/com/amin/beer/order/service/services/BeerOrderManagerImpl.java
@@ -10,6 +10,7 @@ import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
 import org.springframework.statemachine.support.DefaultStateMachineContext;
+import org.springframework.statemachine.support.StateMachineInterceptor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,8 +19,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class BeerOrderManagerImpl implements BeerOrderManager {
     public static final String ORDER_ID_HEADER = "ORDER_ID_HEADER";
 
-    private final StateMachineFactory<BeerOrderStatusEnum, BeerOrderEventEnum> stateMachineFactory;
     private final BeerOrderRepository beerOrderRepository;
+    private final StateMachineFactory<BeerOrderStatusEnum, BeerOrderEventEnum> stateMachineFactory;
+    private final StateMachineInterceptor<BeerOrderStatusEnum, BeerOrderEventEnum> beerOrderStateMachineInterceptor;
 
     @Override
     @Transactional
@@ -33,7 +35,8 @@ public class BeerOrderManagerImpl implements BeerOrderManager {
 
     private void sendBeerOrderEvent(BeerOrder beerOrder, BeerOrderEventEnum eventEnum) {
         StateMachine<BeerOrderStatusEnum, BeerOrderEventEnum> stateMachine = build(beerOrder);
-        Message<BeerOrderEventEnum> message = MessageBuilder.withPayload(eventEnum).build();
+        Message<BeerOrderEventEnum> message = MessageBuilder.withPayload(eventEnum)
+                .setHeader(ORDER_ID_HEADER, beerOrder.getId()).build();
         stateMachine.sendEvent(message);
     }
 
@@ -41,9 +44,11 @@ public class BeerOrderManagerImpl implements BeerOrderManager {
         StateMachine<BeerOrderStatusEnum, BeerOrderEventEnum> stateMachine =
                 stateMachineFactory.getStateMachine(beerOrder.getId());
         stateMachine.stop();
-        stateMachine.getStateMachineAccessor().doWithAllRegions(stateMachineAccess ->
-                stateMachineAccess.resetStateMachine(new DefaultStateMachineContext<>(
-                        beerOrder.getOrderStatus(), null, null, null)));
+        stateMachine.getStateMachineAccessor().doWithAllRegions(stateMachineAccess -> {
+            stateMachineAccess.addStateMachineInterceptor(beerOrderStateMachineInterceptor);
+            stateMachineAccess.resetStateMachine(new DefaultStateMachineContext<>(
+                    beerOrder.getOrderStatus(), null, null, null));
+        });
         stateMachine.start();
         return stateMachine;
     }
--- src/main/java/com/amin/beer/order/service/sm/actions/ValidateOrderAction.java
+++ src/main/java/com/amin/beer/order/service/sm/actions/ValidateOrderAction.java
@@ -1,15 +1,39 @@
 package com.amin.beer.order.service.sm.actions;
 
+import com.amin.beer.order.service.config.JmsConfig;
+import com.amin.beer.order.service.domain.BeerOrder;
 import com.amin.beer.order.service.domain.BeerOrderEventEnum;
 import com.amin.beer.order.service.domain.BeerOrderStatusEnum;
+import com.amin.beer.order.service.repositories.BeerOrderRepository;
+import com.amin.beer.order.service.services.BeerOrderManagerImpl;
+import com.amin.beer.order.service.web.mappers.BeerOrderMapper;
+import com.amin.brewery.model.events.ValidateOrderRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.action.Action;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class ValidateOrderAction implements Action<BeerOrderStatusEnum, BeerOrderEventEnum> {
+    private final BeerOrderRepository beerOrderRepository;
+    private final BeerOrderMapper beerOrderMapper;
+    private final JmsTemplate jmsTemplate;
+
     @Override
     public void execute(StateContext<BeerOrderStatusEnum, BeerOrderEventEnum> stateContext) {
-
+        String beerOrderId = stateContext.getMessage().getHeaders()
+                .get(BeerOrderManagerImpl.ORDER_ID_HEADER, String.class);
+        if (beerOrderId != null) {
+            BeerOrder beerOrder = beerOrderRepository.findOneById(UUID.fromString(beerOrderId));
+            jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_QUEUE,
+                    ValidateOrderRequest.builder().beerOrderDto(beerOrderMapper.beerOrderToDto(beerOrder)).build());
+            log.debug("Sent validation request to queue for order id: " + beerOrderId);
+        }
     }
 }
