b7f0f71036ffae70fe7dd33301e7165b10b34e71
aminmb37 <amin.mb37@gmail.com>
2020-11-10 01:00:07 +0100

    Adding enumerations for Spring State Machine.

--- src/main/java/com/amin/beer/order/service/repositories/BeerOrderRepository.java
+++ src/main/java/com/amin/beer/order/service/repositories/BeerOrderRepository.java
@@ -18,8 +18,8 @@ package com.amin.beer.order.service.repositories;
 
 
 import com.amin.beer.order.service.domain.BeerOrder;
+import com.amin.beer.order.service.domain.BeerOrderStatusEnum;
 import com.amin.beer.order.service.domain.Customer;
-import com.amin.beer.order.service.domain.OrderStatusEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -37,7 +37,7 @@ public interface BeerOrderRepository  extends JpaRepository<BeerOrder, UUID> {
 
     Page<BeerOrder> findAllByCustomer(Customer customer, Pageable pageable);
 
-    List<BeerOrder> findAllByOrderStatus(OrderStatusEnum orderStatusEnum);
+    List<BeerOrder> findAllByOrderStatus(BeerOrderStatusEnum beerOrderStatusEnum);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     BeerOrder findOneById(UUID id);
--- src/main/java/com/amin/beer/order/service/services/BeerOrderServiceImpl.java
+++ src/main/java/com/amin/beer/order/service/services/BeerOrderServiceImpl.java
@@ -18,8 +18,8 @@
 package com.amin.beer.order.service.services;
 
 import com.amin.beer.order.service.domain.BeerOrder;
+import com.amin.beer.order.service.domain.BeerOrderStatusEnum;
 import com.amin.beer.order.service.domain.Customer;
-import com.amin.beer.order.service.domain.OrderStatusEnum;
 import com.amin.beer.order.service.repositories.BeerOrderRepository;
 import com.amin.beer.order.service.repositories.CustomerRepository;
 import com.amin.beer.order.service.web.mappers.BeerOrderMapper;
@@ -84,7 +84,7 @@ public class BeerOrderServiceImpl implements BeerOrderService {
             BeerOrder beerOrder = beerOrderMapper.dtoToBeerOrder(beerOrderDto);
             beerOrder.setId(null); //should not be set by outside client
             beerOrder.setCustomer(customerOptional.get());
-            beerOrder.setOrderStatus(OrderStatusEnum.NEW);
+            beerOrder.setOrderStatus(BeerOrderStatusEnum.NEW);
 
             beerOrder.getBeerOrderLines().forEach(line -> line.setBeerOrder(beerOrder));
 
@@ -109,7 +109,7 @@ public class BeerOrderServiceImpl implements BeerOrderService {
     @Override
     public void pickupOrder(UUID customerId, UUID orderId) {
         BeerOrder beerOrder = getOrder(customerId, orderId);
-        beerOrder.setOrderStatus(OrderStatusEnum.PICKED_UP);
+        beerOrder.setOrderStatus(BeerOrderStatusEnum.PICKED_UP);
 
         beerOrderRepository.save(beerOrder);
     }
